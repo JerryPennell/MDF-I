e1b6a0b8216f2503a06dd47ab74dccc20fb0aec6
Jerry Pennell <jwpenn@fullsail.edu>
2013-03-06 20:13:06 -0500

    Added start images to project
    Launch images and removed ipad version.. not sure if we are supposed to
    have both

---
+++ TestView/TestView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestView
+//
+//  Created by Wayne Pennell on 3/6/13.
+//  Copyright (c) 2013 Wayne Pennell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
