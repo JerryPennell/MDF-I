261092d23fefa64bc5bd329863a76b606731935d
Jerry Pennell <jwpenn@fullsail.edu>
2013-03-19 19:20:15 -0400

    Week 3 Project 3 Tabbed view creation
    Creating initial project

---
+++ Week3/MDFI-Week3Project3/MDFI-Week3Project3/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MDFI-Week3Project3
+//
+//  Created by Wayne Pennell on 3/19/13.
+//  Copyright (c) 2013 Wayne Pennell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
